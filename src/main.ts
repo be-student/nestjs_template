391d80ee061c96b14f3025be1f46a087abc1cd5f
Be-student <presentsong@naver.com>
2022-04-04 20:55:36 +0900

    NestJS template V1.0

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS TEMPLATE')
+    .setDescription('BASIC NESTJS TEMPLATE')
+    .setVersion('1.0')
+    .addTag('NESTJS TEMPLATE')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
