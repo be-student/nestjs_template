c64a0edee0c857319d98012d94605f7f82726c5b
Be-student <presentsong@naver.com>
2022-08-10 21:01:42 +0900

    change configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
