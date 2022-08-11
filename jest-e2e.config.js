466be40403768e801797409fe691d8ec15300112
Be-student <presentsong@naver.com>
2022-08-11 15:09:55 +0900

    feat: jest config to root

---
+++ jest-e2e.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@core/(.*)$': '<rootDir>/core/$1',
+    '^@common/(.*)$': '<rootDir>/common/$1',
+    '^@config/(.*)$': '<rootDir>/config/$1',
+    '^@modules/(.*)$': '<rootDir>/modules/$1',
+  },
+  testTimeout: 60000,
+};
---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coveragePathIgnorePatterns: [],
+  moduleNameMapper: {
+    '^@core/(.*)$': '/src/core/$1',
+    '^@common/(.*)$': '<rootDir>/common/$1',
+    '^@config/(.*)$': '<rootDir>/config/$1',
+    '^@modules/(.*)$': '<rootDir>/modules/$1',
+  },
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  testTimeout: 60000,
+};
