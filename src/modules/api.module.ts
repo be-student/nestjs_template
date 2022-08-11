3daafea8511faabf78860029890db3478015f1a1
Be-student <presentsong@naver.com>
2022-08-11 15:29:48 +0900

    feat(sample): sample module 추가

--- src/modules/api.module.ts
+++ src/modules/api.module.ts
@@ -1,7 +1,8 @@
 import { HealthCheckModule } from '@core/health-check';
 import { Module } from '@nestjs/common';
+import { SampleModule } from './sample';
 
 @Module({
-  imports: [HealthCheckModule],
+  imports: [HealthCheckModule, SampleModule],
 })
 export class ApiModule {}
